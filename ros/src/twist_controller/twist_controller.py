2669e5f62128770385609313a29995058c677f8b
Ashish Rana <ashish.rana.jatt@gmail.com>
2017-09-17 10:21:31 -0700

    FEAT: dbw-node twist_controller

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -4,11 +4,33 @@ ONE_MPH = 0.44704
 
 
 class Controller(object):
-    def __init__(self, *args, **kwargs):
-        # TODO: Implement
-        pass
+    def __init__(self, vehicle_mass, wheel_radius, decel_limit, accel_limit):
+        
+        self.vehicle_mass = vehicle_mass
+        self.decel_limit = decel_limit
+        self.accel_limit = accel_limit
+        self.wheel_radius = wheel_radius
 
-    def control(self, *args, **kwargs):
-        # TODO: Change the arg, kwarg list to suit your needs
-        # Return throttle, brake, steer
-        return 1., 0., 0.
+    def control(self, current_velocity, target_velocity):
+
+    	# Convert to MPH
+    	target_velocity = ONE_MPH * target_velocity
+
+    	# Braking Torque 
+        force = self.vehicle_mass * self.decel_limit
+        torque = force * self.wheel_radius
+
+        # Difference in Target and Actual Velocities
+        del_vel = (target_velocity - current_velocity)
+
+
+        # If Positive difference throttle = nonzero, brake = 0
+        # Else throttle = 0, brake = nonzero
+        if(del_vel > 0):
+        	throttle = 1.0 - (current_velocity/target_velocity)
+        	brake = 0
+        else:
+        	throttle = 0
+        	brake = torque
+
+        return throttle, brake
